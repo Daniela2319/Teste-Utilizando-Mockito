bd0f420da36f037628e698a8279a0fbfda7f34cb
Daniela <frankvt04@gmail.com>
2022-12-09 20:32:45 -0300

    Add files via upload

---
+++ Mockito Teste/MockTest/src/main/java/edu/dani/mockito/ApiDosCorreios.java
@@ -0,0 +1,9 @@
+package edu.dani.mockito;
+
+public class ApiDosCorreios {
+
+    public DadosLocalizacao buscaDadosComBaseNoCep(String cep){
+        return null;
+
+    }
+}
---
+++ Mockito Teste/MockTest/src/main/java/edu/dani/mockito/CadastrarPessoa.java
@@ -0,0 +1,19 @@
+package edu.dani.mockito;
+
+import java.time.LocalDate;
+
+public class CadastrarPessoa {
+
+    private ApiDosCorreios apiDosCorreios;
+
+    public CadastrarPessoa(final ApiDosCorreios apiDosCorreios1){
+        this.apiDosCorreios = apiDosCorreios1;
+    }
+
+    public Pessoa cadastrarPessoa(String nome, String documento, LocalDate nascimento, String cep){
+        Pessoa pessoa = new Pessoa(nome, documento, nascimento);
+        DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscaDadosComBaseNoCep(cep);
+        pessoa.adicionaDadosDeEndereco(dadosLocalizacao);
+        return pessoa;
+    }
+}
